c2eb15f37f7951d0dd242cd990b751442a4cb56a
jaywant1999 <153539866+jaywant1999@users.noreply.github.com>
2024-09-01 22:22:41 +0530

    last commit

--- digitalflake/Express/Router.js
+++ digitalflake/Express/Router.js
@@ -13,15 +13,37 @@ const getAllProducts = async (req, res) => {
     res.json(products);
 };
 
-const saveProducts = async (req, res) => {
+const saveProduct = async (req, res) => {
     const data = req.body;
     const newProduct = await Product.create(data);
     res.json(newProduct);
-    console.log("successfull",data);
+    console.log("Successfully save : ",data);
 };
+
+const deleteProduct = async (req, res) => {
+    const id = req.params.id;
+    const deletePro = await Product.destroy({
+        where:{
+        id:id
+    }});
+    res.json(deletePro);
+};
+
+const updateProduct = async (req, res) => {
+    const data = req.body;
+    const Id = req.body;
+    const updateProduct = {...data};
+    const updateCount = await Product.update(updateProduct, {
+        where :{Id},
+    });
+    res.json(updateCount)
+}
 
 router.get('/products', getAllProducts);
 router.get('/products/:id', getProduct);
-router.post('/products', saveProducts);
+router.post('/products', saveProduct);
+router.put('/products/:id', updateProduct);
+router.delete('/products/:id', deleteProduct);
+
 
 module.exports = router;
